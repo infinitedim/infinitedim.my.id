ac4e46b8d5f48e25143d965032e94a555ae7ffdf
infinitedim <saputradimas.boediluhur@gmail.com>
2022-04-14 16:38:55 +0700

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,14 +17,14 @@ const About = () => {
           </Col>
           <Col lg='8' md='9'>
             <div className="about__content__wrapper d-flex gap-5 align-items-center">
-              <div className="about__img w-100">
+              <div className="about__img w-25">
                 <img src="https://images.unsplash.com/photo-1649773965074-26c817bc7636?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="" className="w-100" />
               </div>
               <div className="about__content">
                 <h2>I'm Dimas Saputra</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum adipisci commodi dolore tenetur ad. Adipisci vero cupiditate quia, optio incidunt est. Quas eveniet distinctio explicabo molestias nesciunt, dolore labore maxime!</p>
               <div className="social__links">
-                <h6 className="mb-2">Connect With Me</h6>
+                <h6 className="mb-3">Connect With Me</h6>
                 <span><a href="#"><i class="ri-instagram-line"></i></a></span>
                 <span><a href="#"><i class="ri-facebook-line"></i></a></span>
                 <span><a href="#"><i class="ri-youtube-line"></i></a></span>
---
+++ src/components/Education.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Education = () => {
+  return <table className="table">
+    <thead>
+      <tr>
+        <th>Degree</th>
+        <th>Departement</th>
+        <th>Institute</th>
+        <th>Year</th>
+      </tr>
+    </thead>
+    <thead>
+      <tr>
+        <th>Degree</th>
+        <th>Departement</th>
+        <th>Institute</th>
+        <th>Year</th>
+      </tr>
+    </thead>
+  </table>
+}
+
+export default Education
