73baba7d5cceb1ef5cab6337bbef68e02fc86ac9
infinitedim <saputradimas.boediluhur@gmail.com>
2022-04-16 13:56:32 +0700

    adding ssr

--- src/index.js
+++ src/index.js
@@ -17,11 +17,8 @@ import './scss/index.scss';
 
 // import './scss/index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.hydrate(
+  <App />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
