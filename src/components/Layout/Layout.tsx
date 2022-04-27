ad6ac14d968b6641e285024da00b3860b9deb387
infinitedim <saputradimas.boediluhur@gmail.com>
2022-04-27 19:34:24 +0700

    services init

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,15 +3,19 @@ import Header from '../Header'
 import Hero from '../Hero'
 import About from '../About'
 import Services from '../Services'
+import Footer from '../Footer'
 
 const Layout = () => {
 
-  return <React.Fragment>
-    <Header />
-    <Hero />
-    <About />
-    <Services />
-  </React.Fragment>
+  return (
+    <React.Fragment>
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <Footer />
+    </React.Fragment>
+  )
 }
 
 export default Layout
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,8 +1,83 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+
+interface ServicesTypes {
+  icon: string;
+  title: string;
+  desc: string;
+}
+
+const servicesData: ServicesTypes[] = [
+  {
+    icon: 'ri-code-line',
+    title: 'Lorem Ipsum',
+    desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Daleniti dolorem quae voluptatum officia at repudiandae suscipit exercitationem dolor asperiores cum?'
+  },
+  {
+    icon: 'ri-code-line',
+    title: 'Lorem Ipsum',
+    desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Daleniti dolorem quae voluptatum officia at repudiandae suscipit exercitationem dolor asperiores cum?'
+  },
+  {
+    icon: 'ri-code-line',
+    title: 'Lorem Ipsum',
+    desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Daleniti dolorem quae voluptatum officia at repudiandae suscipit exercitationem dolor asperiores cum?'
+  },
+  {
+    icon: 'ri-code-line',
+    title: 'Lorem Ipsum',
+    desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Daleniti dolorem quae voluptatum officia at repudiandae suscipit exercitationem dolor asperiores cum?'
+  }, 
+]
+
+type ServicesItemProps = {
+  icon: string,
+  title: string,
+  desc: string,
+}
+
+const ServicesItem = ({
+  icon,
+  title,
+  desc,
+}: ServicesItemProps): React.ReactElement => {
+  return (
+    <Col lg="4" md="6" sm="6">
+      <div className="services__card">
+        <span className="services__icon">
+          <i className={icon}></i>
+        </span>
+        <h2>{title}</h2>
+        <p>{desc}</p>
+      </div>
+    </Col>
+    )
+}
 
 const Services = () => {
   return (
-    <div>services</div>
+    <section id="services">
+      <Container>
+        <Row>
+          <Col lg="12" className="services__top mb-5">
+            <h6>Features</h6>
+            <h2>What Service I Provide</h2>
+          </Col>
+          {
+            servicesData.map((item,index) => {
+              return (
+              <ServicesItem
+                key={index.toString()}
+                icon={item.icon}
+                title={item.title}
+                desc={item.desc}
+              />
+              )
+            })
+          }
+        </Row>
+      </Container>
+    </section>
     )
 }
 
