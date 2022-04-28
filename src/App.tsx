617a7bbf5510e68d8ae90b5f09d6650dfbd14080
infinitedim <saputradimas.boediluhur@gmail.com>
2022-04-28 18:24:24 +0700

    adding development alert and fixing navbar desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import Layout from "./components/Layout/Layout";
 import Swal from 'sweetalert2';
-
+import {useRef, useEffect} from 'react'
 
 function App() {
+  const loadRef = useRef<HTMLElement>(null);
+ 
+  useEffect(() => {
+    window?.addEventListener('load', () => {
+      Swal.fire({
+        icon: "error",
+        title: "ごめん😕",
+        text: "このページは開発中です",
+        color: "#7765ff",
+        background: "#102048"
+      })
+    });
+  }, []);
   return (
-    <Layout onLoad="warn()" />
+    <Layout ref={loadRef}/>
     );
 }
 
